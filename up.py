60e3d12f532214ffbcddd03f80f1b526b1db69cb
ArnaudVella <36851494+ArnaudVella@users.noreply.github.com>
2018-06-27 08:58:46 +0200

    Update up.py

--- up.py
+++ up.py
@@ -35,41 +35,49 @@ def upload():
 	img = Image.open('static/vide.png')
 	img.save('static/fig.png', "PNG")
 	
+	@app.route('/up/', methods=['GET', 'POST'])
+def upload():
+	img = Image.open('static/vide.png')
+	img.save('static/fig.png', "PNG")
+	
 	if request.method == 'POST':
+		if 'fic' not in request.files:	# on vérifie qu'un fichier a bien été envoyé
+            		flash(u'Vous avez oublié le fichier !', 'error')
+            		return redirect(request.url)		
+		
+
 		img = request.files['fic']
 		
-		if img: # on vérifie qu'un fichier a bien été envoyé
-			if extension_ok(img.filename): # on vérifie que son extension est valide
-				nom = secure_filename(img.filename)
-				img.save('./static/' + nom)			#sauvegarde/ouverture nécessaire pour img.size
-				img = Image.open('./static/' + nom)
-				os.remove('./static/' + nom)
-				
-				"""redimensionnement"""
-				width, height = img.size
-				if width > height:
-					left = (width - height) / 2
-					right = width - left
-					top = 0
-					bottom = height
-				else:
-					top = (height - width) / 2
-					bottom = height - top
-					left = 0
-					right = width
-				img = img.crop((left, top, right, bottom))
-				img = img.resize([224, 224], Image.ANTIALIAS)
-				
-				"""conversion en png"""
-				img.save('static/img.png',"PNG")
-				flash(u'Image envoyée')
-				final()
-				return render_template('up_up.html')
-				
+		if extension_ok(img.filename): # on vérifie que son extension est valide
+			nom = secure_filename(img.filename)
+			img.save('./static/' + nom)			#sauvegarde/ouverture nécessaire pour img.size
+			img = Image.open('./static/' + nom)
+			os.remove('./static/' + nom)
+			
+			"""redimensionnement"""
+			width, height = img.size
+			if width > height:
+				left = (width - height) / 2
+				right = width - left
+				top = 0
+				bottom = height
 			else:
-				flash(u'Ce fichier ne porte pas une extension autorisée !', 'error')
+				top = (height - width) / 2
+				bottom = height - top
+				left = 0
+				right = width
+			img = img.crop((left, top, right, bottom))
+			img = img.resize([224, 224], Image.ANTIALIAS)
+			
+			"""conversion en png"""
+			img.save('static/img.png',"PNG")
+			flash(u'Image envoyée')
+			final()
+			return render_template('up_up.html')
+			
 		else:
-			flash(u'Vous avez oublié le fichier !', 'error')  # ne fonctionne pas!!!!
+			flash(u'Ce fichier ne porte pas une extension autorisée !', 'error')
+		
 			
 
 
